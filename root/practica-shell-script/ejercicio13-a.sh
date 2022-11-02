4273e8a7ac5b0e720299698597e594b434c37e1d
Guillermo Helfer <89919447+GuillermoHelfer@users.noreply.github.com>
2022-11-01 22:38:54 -0300

    Delete root directory

--- root/practica-shell-script/ejercicio13-a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Realizar un script que visualice por pantalla los numeros del 1 al 100 asi como sus cuadrados
-
-for ((i=1; i <= 100; i++)) do
-	echo "$i"
-	expr $i \* $i
-	sleep 1
-done
--- root/practica-shell-script/ejercicio18.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# verifica cada 10 segundos si un usuario esta logueado e informa cuando se loguea
-
-until who | grep -q "$1"
-do
-	sleep 10
-done
-
-echo "Usuario $1 logueado en el sistema"
